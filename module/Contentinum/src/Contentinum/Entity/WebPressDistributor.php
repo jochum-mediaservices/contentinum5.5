e02461c563518eb0f7d0dd1d83e212f0ec092017
Mikel1961 <michael.jochum@jochum-mediaservices.de>
2017-01-12 15:29:27 +0100

    press distribution entities

---
+++ module/Contentinum/src/Contentinum/Entity/WebPressDistributor.php
@@ -0,0 +1,272 @@
+<?php
+/**
+ * contentinum - accessibility websites
+ *
+ * LICENSE
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category Mcevent
+ * @package Entity
+ * @author Michael Jochum, michael.jochum@jochum-mediaservices.de
+ * @copyright Copyright (c) 2009-2013 jochum-mediaservices, Katja Jochum (http://www.jochum-mediaservices.de)
+ * @license http://www.opensource.org/licenses/bsd-license
+ * @since contentinum version 5.0
+ * @link      https://github.com/Mikel1961/contentinum-components
+ * @version   1.0.0
+ */
+namespace Contentinum\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ContentinumComponents\Entity\AbstractEntity;
+
+/**
+ * WebPressDistributor
+ *
+ * @ORM\Table(name="web_press_distributor")
+ * @ORM\Entity
+ */
+class WebPressDistributor extends AbstractEntity
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     */
+    private $id;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=250, nullable=false)
+     */
+    private $name; // varchar(50) DEFAULT NULL,  
+    
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="distributor_list", type="text", nullable=false)
+     */
+    private $distributorList = '';    
+    
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="publish", type="string", length=10, nullable=false)
+     */
+    private $publish = 'no'; // varchar(10) NOT NULL DEFAULT 'no',    
+    
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="created_by", type="integer", nullable=false)
+     */
+    private $createdBy = '0';
+    
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="update_by", type="integer", nullable=false)
+     */
+    private $updateBy = '0';    
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="register_date", type="datetime", nullable=false)
+     */
+    private $registerDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="up_date", type="datetime", nullable=false)
+     */
+    private $upDate;
+
+    /**
+     * Construct
+     * @param array $options
+     */
+    public function __construct (array $options = null)
+    {
+        if (is_array($options)) {
+            $this->setOptions($options);
+        }
+    }
+    
+    /** (non-PHPdoc)
+     * @see \ContentinumComponents\Entity\AbstractEntity::getEntityName()
+     */
+    public function getEntityName()
+    {
+        return get_class($this);
+    }
+    
+    /** (non-PHPdoc)
+     * @see \ContentinumComponents\Entity\AbstractEntity::getPrimaryKey()
+     */
+    public function getPrimaryKey()
+    {
+        return 'id';
+    }
+    
+    /** (non-PHPdoc)
+     * @see \ContentinumComponents\Entity\AbstractEntity::getPrimaryValue()
+     */
+    public function getPrimaryValue()
+    {
+        return $this->id;
+    }
+    
+    /** (non-PHPdoc)
+     * @see \ContentinumComponents\Entity\AbstractEntity::getProperties()
+     */
+    public function getProperties()
+    {
+        return get_object_vars($this);
+    }
+	/**
+     * @return the $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+	/**
+     * @param number $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+
+    /**
+     * @return the $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return the $distributorList
+     */
+    public function getDistributorList()
+    {
+        return $this->distributorList;
+    }
+
+    /**
+     * @param string $distributorList
+     */
+    public function setDistributorList($distributorList)
+    {
+        $this->distributorList = $distributorList;
+    }
+
+    /**
+     * @return the $publish
+     */
+    public function getPublish()
+    {
+        return $this->publish;
+    }
+
+    /**
+     * @param string $publish
+     */
+    public function setPublish($publish)
+    {
+        $this->publish = $publish;
+    }
+
+    /**
+     * @return the $createdBy
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * @param number $createdBy
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+    }
+
+    /**
+     * @return the $updateBy
+     */
+    public function getUpdateBy()
+    {
+        return $this->updateBy;
+    }
+
+    /**
+     * @param number $updateBy
+     */
+    public function setUpdateBy($updateBy)
+    {
+        $this->updateBy = $updateBy;
+    }
+
+    /**
+     * @return the $registerDate
+     */
+    public function getRegisterDate()
+    {
+        return $this->registerDate;
+    }
+
+	/**
+     * @param DateTime $registerDate
+     */
+    public function setRegisterDate($registerDate)
+    {
+        $this->registerDate = $registerDate;
+    }
+
+	/**
+     * @return the $upDate
+     */
+    public function getUpDate()
+    {
+        return $this->upDate;
+    }
+
+	/**
+     * @param DateTime $upDate
+     */
+    public function setUpDate($upDate)
+    {
+        $this->upDate = $upDate;
+    }
+
+}
